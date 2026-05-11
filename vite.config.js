041a11b9a4d5db7e02673c96a1f5f4f149facb02
Barbara Larissa <laribdev@gmail.com>
2026-05-10 22:54:31 -0300

    resolvendo tela branca de vez

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/luxe-seoul/', // Force o nome do repo aqui para o deploy
+  base: '/', 
 })
